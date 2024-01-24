3d7bc1ea37cd147280619f2708a19d0788e767c8
mkippes <mkippes@motomec.com.ar>
2024-01-24 09:06:00 -0300

    add text

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hola, Coder!</Text>
+      <Text>Este es mi primer desafio.</Text>
       <StatusBar style="auto" />
     </View>
   );
