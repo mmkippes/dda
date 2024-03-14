04cbcb6f125294eda8bbdbd697fe62deeb1bcd33
mkippes <mkippes@motomec.com.ar>
2024-03-14 12:12:59 -0300

    manejo de persmisos

--- src/components/Camera.js
+++ src/components/Camera.js
@@ -1,16 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Camera } from 'expo-camera';
-import { View, Button } from 'react-native';
+import { View, Button, Alert } from 'react-native';
 
 const CameraScreen = () => {
+    const [hasPermission, setHasPermission] = useState(null);
     const cameraRef = useRef(null);
 
+    useEffect(() => {
+        (async () => {
+            const { status } = await Camera.requestCameraPermissionsAsync();
+            setHasPermission(status === 'granted');
+        })();
+    }, []);
+
     const takePicture = async () => {
+        if (!hasPermission) {
+            console.log('Los permisos de la cámara fueron rechazados, favor de revisar los mismos en la configuración de tu celular.');
+            return;
+        }
         if (cameraRef.current) {
             const { uri } = await cameraRef.current.takePictureAsync();
             console.log('Foto tomada:', uri);
         }
     };
+
+    if (hasPermission === null) {
+        return <View />;
+    }
+    if (hasPermission === false) {
+        return (
+            <View>
+                <Text>Se rechazaron los permisos de la cámara.</Text>
+            </View>
+        );
+    }
 
     return (
         <View style={{ flex: 1 }}>
